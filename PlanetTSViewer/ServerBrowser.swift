4c450cdd8b372eee7e7b7e14d6cf2bd327d81622
Felix <felix@Supers-iMac.fritz.box>
2015-06-14 21:57:24 +0200

    - added server browser

---
+++ PlanetTSViewer/ServerBrowser.swift
@@ -0,0 +1,204 @@
+//
+//  ServerBrowser.swift
+//  PlanetTSViewer
+//
+//  Created by Felix on 14/06/15.
+//  Copyright (c) 2015 wz. All rights reserved.
+//
+
+import Foundation
+
+protocol ServerBrowserDelegate : class {
+    func serverBrowserLoadedServers(serverBrowser: ServerBrowser)
+}
+
+class ServerInfo : NSObject {
+    
+    enum Membership: Int {
+        case Basic = 0
+        case Member
+        case Premium
+        
+        init(value: Int) {
+            if value >= Membership.Basic.rawValue && value <= Membership.Premium.rawValue {
+                self = Membership(rawValue: value)!
+            }
+            else {
+                self = .Basic
+            }
+        }
+    }
+    
+    var address: String = ""
+    var country: String = ""
+    var name: String = ""
+    var membership: Membership = .Basic
+    var slots: Int = 0
+    var users: Int = 0
+    var publicChannels: Bool = false
+    var online: Bool = false
+    var password: Bool = false
+    
+    func takeValuesFromJSONObject(json: NSDictionary) {
+        
+        if let address = json["address"] as? String {
+            self.address = address
+        }
+        
+        if let country = json["country"] as? String {
+            self.country = country
+        }
+        
+        if let name = json["name"] as? String {
+            self.name = name
+        }
+        
+        if let membership = json["membership"] as? NSNumber {
+            self.membership = Membership(value: membership.integerValue)
+        }
+        
+        if let slots = json["slots"] as? NSNumber {
+            self.slots = slots.integerValue
+        }
+        
+        if let users = json["users"] as? NSNumber {
+            self.users = users.integerValue
+        }
+        
+        if let publicChannels = json["channels"] as? NSNumber {
+            self.publicChannels = publicChannels.boolValue
+        }
+        
+        if let online = json["online"] as? NSNumber {
+            self.online = online.boolValue
+        }
+        
+        if let password = json["password"] as? NSNumber {
+            self.password = password.boolValue
+        }
+    }
+
+}
+
+class ServerBrowser : NSObject {
+    
+    weak var delegate: ServerBrowserDelegate?
+    var servers: [ServerInfo] = []
+    
+    override init() {
+        super.init()
+    }
+    
+    func requestServers() {
+        
+        let url = NSURL(string: "https://api.planetteamspeak.com/serverlist/?country=de&limit=10")!
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.downloadTaskWithURL(url, completionHandler: handleDownloadTask)
+        println("starting download task  for server list \(url)")
+        task.resume()
+
+    }
+    
+    private func handleDownloadTask(location: NSURL?, response: NSURLResponse?, error: NSError?) {
+        if let actualLocation = location {
+            println("downlaod server list task completed")
+            
+            if let data = NSData(contentsOfURL: actualLocation) {
+                println("response size in bytes: \(data.length)")
+                if let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSDictionary {
+                    println(json)
+                    if let newServers = self.loadJSONObject(json) {
+                        dispatch_async(dispatch_get_main_queue()) { //load the data on the main thread
+                            self.servers = newServers
+                            self.delegate?.serverBrowserLoadedServers(self)
+                        }
+                    }
+                    else {
+                        println("can't load servers from json")
+                    }
+                }
+                else {
+                    println("can't get json data from response. error: \(error)")
+                }
+            }
+        }
+        
+        if let actualError = error {
+            println("download task error \(actualError.code): \(actualError.localizedDescription)")
+        }
+    }
+    
+    private func loadJSONObject(json: NSDictionary) -> [ServerInfo]? {
+        let result: NSDictionary
+        let data: NSArray
+        
+        if let actualResult = json["result"] as? NSDictionary {
+            result = actualResult
+        }
+        else {
+            println("no result object inside json data")
+            return nil
+        }
+        
+        if let actualData = result["data"] as? NSArray {
+            data = actualData
+        }
+        else {
+            println("no data object inside json")
+            return nil
+        }
+        
+        println("json data node count: \(data.count)")
+        
+        var servers = [ServerInfo]()
+        
+        for node in data as! [NSDictionary] {
+            var serverInfo = ServerInfo()
+            serverInfo.takeValuesFromJSONObject(node)
+            
+            servers.append(serverInfo)
+        }
+        
+        return servers
+//
+//        for node in data as! [NSDictionary] {
+//            let parentIdent: String
+//            if let parent = node["parent"] as? String {
+//                parentIdent = parent
+//            }
+//            else {
+//                println("node has no parent")
+//                continue
+//            }
+//            
+//            let tsNode: TSNode
+//            
+//            if let parent = nodeWithIdentifier(parentIdent) {
+//                tsNode = TSNode()
+//                parent.children.append(tsNode)
+//            }
+//            else {
+//                if type != .Invalid {
+//                    println("found node with no parent and root is already initialized")
+//                    continue
+//                }
+//                tsNode = self
+//            }
+//            
+//            tsNode.takeValuesFromJSONObject(node)
+//            
+//            if let server = tsNode as? TSServer where server.showServerInTree == false {
+//                // don't add server itself to the tree
+//            }
+//            else {
+//                allNodes.append(Weak(tsNode))
+//            }
+//            
+//            if tsNode.iconId != 0 {
+//                requestIconForNode(tsNode)
+//            }
+//        }
+    }
+
+}
---
+++ PlanetTSViewer/ServerBrowserViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ServerBrowserViewController.swift
+//  PlanetTSViewer
+//
+//  Created by Felix on 14/06/15.
+//  Copyright (c) 2015 wz. All rights reserved.
+//
+
+import UIKit
+
+class ServerBrowserViewController: UITableViewController, ServerBrowserDelegate {
+
+    var serverBrowser = ServerBrowser()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        serverBrowser.delegate = self
+        serverBrowser.requestServers()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func serverBrowserLoadedServers(serverBrowser: ServerBrowser) {
+        println("server browser loaded servers")
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return serverBrowser.servers.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("serverCell", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let serverInfo = serverBrowser.servers[indexPath.row]
+        
+        cell.textLabel!.text = serverInfo.name
+        cell.detailTextLabel!.text = "\(serverInfo.address)  users \(serverInfo.users) / \(serverInfo.slots)"
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
